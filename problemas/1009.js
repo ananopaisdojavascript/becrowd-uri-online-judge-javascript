9b110f9114014e7da144a18c77f53ddf6d4d073c
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-15 20:21:07 -0300

    1009

---
+++ problemas/1009.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('../stdin', 'utf8');
+const lines = input.split('\n');
+
+const getSalaryWithBonus = (salary, sales) => {
+    return salary + (sales * 0.15);
+};
+
+const salary = parseFloat(lines[1]);
+const totalSales = parseFloat(lines[2]);
+const total = getSalaryWithBonus(salary, totalSales);
+
+console.log(`TOTAL = R$ ${total.toFixed(2)}`);
