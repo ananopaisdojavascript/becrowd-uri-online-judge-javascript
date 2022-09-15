cc6d78b3feee8b7c6e82a401e7c75c3c931a7e86
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-14 22:01:32 -0300

    1006

--- problemas/2694.js
+++ problemas/2694.js
@@ -1,11 +1,35 @@
-const lines = prompt("Digite os valores de entrada").split("\n");
-console.log(lines)
+// const input = require('fs').readFileSync('../stdin', 'utf8');
+// const lines = input.split('\n');
+
+// console.log(input)
+// const sum = (a, b, c) => a + b + c;
+
+// const cases = lines.shift();
+// console.log(cases)
+// console.log(lines)
+
+// for (let i = 0; i < cases; i++) {
+//   const strToNum = lines[i].replace(/[a-zA-Z ]/g, '')
+//   const num1 = Number(strToNum.slice(0, 2));
+//   const num2 = Number(strToNum.slice(2, 5));
+//   const num3 = Number(strToNum.slice(5));
+//   console.log(num1)
+// }
+
 
 // const sum = (a, b, c) => a + b + c;
-const cases = lines.length;
-console.log(cases)
+// const cases = lines.length;
+// console.log(cases)
 
-for (let i = 0; i < cases; i++) {
-  const strToNum = lines[i].replace(/[a-zA-Z ]/g, '')
-  console.log(strToNum)
-}
+// for (let i = 0; i < cases; i++) {
+//   const strToNum = lines[i].replace(/[a-zA-Z ]/g, '')
+//   console.log(strToNum)
+// }
+
+const str1 = 'Ab23s249ttux2';
+const str2 = 'Abx3s249ttu2x';
+const str3 = 'Ab23s23xttu21';
+
+console.log(str1.replace(/[a-zA-Z ]/g, ''))
+console.log(str2.replace(/[a-zA-Z ]/g, ''))
+console.log(str3.replace(/[a-zA-Z ]/g, ''))
