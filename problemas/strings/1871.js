180b24f32dcbe5cca6dcb6b3a37e1578c302d2d5
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-16 21:59:32 -0300

    Zero vale Zero

--- problemas/strings/1871.js
+++ problemas/strings/1871.js
@@ -1,8 +1,14 @@
 const input = require("fs").readFileSync("../../stdin", "utf8");
 const lines = input.split("\n");
 
-let tag = lines.pop();
+console.log(lines)
+const getSum = (a, b) => a + b;
 
-while(tag) {
-    tag = lines.pop(); 
+while(lines.length){
+    let [A, B] = lines.shift().split(" ").map(Number);
+    if(A === 0 && B === 0) {
+        break
+    }
+    const sum = getSum(A, B)
+    console.log(sum.toString().replace(/0/g, ''))
 }
